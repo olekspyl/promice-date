f1b25f60a9bcad4780ad4c0bff020295caf577ce
Oleksandra Pylypchuk <lemocream@gmail.com>
2023-07-12 14:02:53 +0200

    end timer

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,5 @@
 import flatpickr from 'flatpickr';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import 'flatpickr/dist/flatpickr.min.css';
 
 const inputEl = document.querySelector('#datetime-picker');
@@ -9,6 +10,8 @@ const minutes = document.querySelector('span[data-minutes]');
 const seconds = document.querySelector('span[data-seconds]');
 
 btnStart.setAttribute('disabled', 'disabled');
+btnStart.addEventListener('click', onBtnStartClick);
+
 let selectedDate;
 let currentDate;
 
@@ -17,34 +20,53 @@ const options = {
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
+
   onClose(selectedDates) {
-    selectedDate = selectedDates[0].getTime();
-    currentDate = options.defaultDate.getTime();
-    if (selectedDate > currentDate) {
-      btnStart.removeAttribute('disabled');
-      return selectedDate;
-    } else {
-      window.alert('Please choose a date in the future');
-    }
+    timerId = setInterval(() => {
+      selectedDate = selectedDates[0].getTime();
+      currentDate = new Date().getTime();
+    }, 1000);
+
+    setTimeout(() => {
+      if (selectedDate > currentDate) {
+        btnStart.removeAttribute('disabled');
+        return selectedDate;
+      } else {
+        // window.alert('Please choose a date in the future');
+        Notify.failure('Please choose a date in the future');
+        btnStart.setAttribute('disabled', 'disabled');
+      }
+    }, 1000);
   },
 };
 
-const endDate = flatpickr(inputEl, options);
-
-btnStart.addEventListener('click', onBtnStartClick);
+flatpickr(inputEl, options);
 
 function onBtnStartClick() {
-  const convertedMs = convertMs(selectedDate - currentDate);
   timerId = setInterval(() => {
+    const convertedMs = convertMs(selectedDate - currentDate);
     days.textContent = addLeadingZero(convertedMs.days, 3, '00');
     hours.textContent = addLeadingZero(convertedMs.hours, 2, '0');
     minutes.textContent = addLeadingZero(convertedMs.minutes, 2, '0');
     seconds.textContent = addLeadingZero(convertedMs.seconds, 2, '0');
+
+    timerClose(
+      Number(days.textContent),
+      Number(hours.textContent),
+      Number(minutes.textContent),
+      Number(seconds.textContent)
+    );
   }, 1000);
 }
 
 function addLeadingZero(value, targetLength, padString) {
   return value.toString().padStart(targetLength, padString);
+}
+
+function timerClose(days, hours, minutes, seconds) {
+  if (!(days + hours + minutes + seconds)) {
+    clearInterval(timerId);
+  }
 }
 
 function convertMs(ms) {
